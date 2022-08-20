0554ec9e92d2bce213bc98253e29e5e3befb1fda
dalbirsrana <dalbirsrana@gmail.com>
2022-08-19 19:30:50 -0700

    Updates

---
+++ precache-manifest.69dd615ae93970f12677219a1825a7ee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63071d77c7915cd0630e4dc672fb5343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "829d47cf56ef27dd1638",
+    "url": "/static/css/main.a96101a5.chunk.css"
+  },
+  {
+    "revision": "d1afd75ad7bd4a5529ce",
+    "url": "/static/js/2.395f9962.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.395f9962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829d47cf56ef27dd1638",
+    "url": "/static/js/main.60248656.chunk.js"
+  },
+  {
+    "revision": "17b55aa52587cb70257e",
+    "url": "/static/js/runtime-main.5d2bdb24.js"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/static/media/006-react.eab76719.svg"
+  },
+  {
+    "revision": "b018dd4ee2594a6b20da9b88e21dd5e5",
+    "url": "/static/media/021-mysql.b018dd4e.svg"
+  },
+  {
+    "revision": "3023657df2a80a0f8957a339d14ef515",
+    "url": "/static/media/026-sass.3023657d.svg"
+  },
+  {
+    "revision": "d956cbbb2e59777272370c854a3b6747",
+    "url": "/static/media/027-php.d956cbbb.svg"
+  },
+  {
+    "revision": "e169ba293cabaff0dc96397aed47a250",
+    "url": "/static/media/029-javascript.e169ba29.svg"
+  },
+  {
+    "revision": "15a5a8bda0c1e730557fa45b6a2a941c",
+    "url": "/static/media/030-html-5.15a5a8bd.svg"
+  },
+  {
+    "revision": "55338d57e6f6c200bcd23f0af4710a9b",
+    "url": "/static/media/031-css.55338d57.svg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/static/media/032-nodejs.be2df796.svg"
+  },
+  {
+    "revision": "fde4cdf4b945545f5a118e34b4a7cf2b",
+    "url": "/static/media/Resume-Dalbir.fde4cdf4.pdf"
+  },
+  {
+    "revision": "a46e500df609ead253c6698da4d16ce1",
+    "url": "/static/media/pattern-geometry-2.90856b56.png"
+  },
+  {
+    "revision": "2544dd9edc414b7088dee72c8ae52f2b",
+    "url": "/static/media/wordpress.2544dd9e.svg"
+  }
+]);
